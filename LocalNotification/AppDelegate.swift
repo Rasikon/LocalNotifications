b4e211f550cc91aa6b6354a855046fcfa05b5dfe
Ivan Redreev <redreev.i@richpeach.pro>
2021-12-02 16:41:30 +0500

    CreateLocalNotification

--- LocalNotification/AppDelegate.swift
+++ LocalNotification/AppDelegate.swift
@@ -6,31 +6,44 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    public let notificationCenter = UNUserNotificationCenter.current()
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { success, error in
+            
+            guard success else { return }
+            self.notificationCenter.getNotificationSettings { settings in
+                guard settings.authorizationStatus == .authorized else { return }
+            }
+        }
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    public func scheduleNotification(notificationType: String) {
+        
+        let notificationContent = UNMutableNotificationContent()
+        
+        notificationContent.title = notificationType
+        notificationContent.body = "Test Notification"
+        notificationContent.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "Local Notification", content: notificationContent, trigger: trigger)
+        
+        notificationCenter.add(request) { error in
+            if let error = error {
+                print("\(error.localizedDescription)")
+            }
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public final class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet private weak var getPushButton: UIButton!
+    
+    
+    // MARK: Private propertise
+    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    
+    // MARK: Lifecycle
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        applyStyle()
     }
-
-
+    
+    
+    // MARK: Actions
+    @IBAction private func pressGetPushButton(_ sender: UIButton) {
+        launchAlert()
+    }
+    
+    
+    // MARK: Private methods
+    private func applyStyle() {
+        getPushButton.layer.cornerRadius = 10
+    }
+    
+    private func launchAlert() {
+        let title = "Local Notification"
+        let alert = UIAlertController(title: title, message: "A notification will be sent in 10 seconds", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { action in
+            self.appDelegate?.scheduleNotification(notificationType: title)
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
